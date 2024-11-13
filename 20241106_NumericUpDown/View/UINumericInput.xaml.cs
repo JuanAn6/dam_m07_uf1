6040860d6af2dc97627fde5f9ffe6939e2819986
JuanAn6 <juanantoniorizija666@gmail.com>
2024-11-13 15:10:00 +0100

    13-11-2024

---
+++ 20241106_NumericUpDown/View/UINumericInput.xaml.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _20241106_NumericUpDown.View
+{
+    public sealed partial class UINumericInput : UserControl
+    {
+
+
+
+        public int Valor
+        {
+            get { return (int)GetValue(ValorProperty); }
+            set { SetValue(ValorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Valor.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ValorProperty =
+            DependencyProperty.Register("Valor", typeof(int), typeof(UINumericInput), new PropertyMetadata(0, ValorChangedCallbackStatic));
+
+        //Modificador del valor desde fora i desde dins s'executa aixo!
+        private static void ValorChangedCallbackStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UINumericInput control = (UINumericInput)d;
+            control.ValorChangedCallback(d,e);
+        }
+
+        private void ValorChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Valor = Math.Clamp(Valor, Min, Max);
+            showNumero();
+        }
+
+        public int Max
+        {
+            get { return (int)GetValue(MaxProperty); }
+            set { SetValue(MaxProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Max.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MaxProperty =
+            DependencyProperty.Register("Max", typeof(int), typeof(UINumericInput), new PropertyMetadata(100, MaxChangedCallbackStatic));
+
+        private static void MaxChangedCallbackStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UINumericInput control = (UINumericInput)d;
+            control.MaxMinChangedCallback(d, e);
+        }
+
+
+        public int Min
+        {
+            get { return (int)GetValue(MinProperty); }
+            set { SetValue(MinProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Min.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty MinProperty =
+            DependencyProperty.Register("Min", typeof(int), typeof(UINumericInput), new PropertyMetadata(0, MinChangedCallbackStatic));
+
+        private static void MinChangedCallbackStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UINumericInput control = (UINumericInput)d;
+            control.MaxMinChangedCallback(d, e);
+        }
+
+        private void MaxMinChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Valor = Math.Clamp(Valor, Min, Max); //Math.Min(Max, Math.Min(Min, Valor));
+        }
+
+        public UINumericInput()
+        {
+            this.InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            showNumero();
+        }
+
+        private void btnUp_Click(object sender, RoutedEventArgs e)
+        {
+            Valor++;
+        }
+
+        private void btnDown_Click(object sender, RoutedEventArgs e)
+        {
+            Valor--;
+        }
+
+        private void showNumero()
+        {
+            tbNum.Text = Valor + "";
+        }
+
+        private void tbNum_TextChanging(TextBox sender, TextBoxTextChangingEventArgs args)
+        {
+            int res;
+            if(int.TryParse(sender.Text, out res))
+            {
+                Valor = res;
+            }
+            showNumero();
+        }
+
+       
+    }
+}
---
+++ 20241107_RaitingControl/View/UIRaiting.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace _20241107_RaitingControl.View
+{
+    public sealed partial class UIRaiting : UserControl
+    {
+
+        private const int CAMELOS = 5;
+        public UIRaiting()
+        {
+            this.InitializeComponent();
+        }
+
+        private bool loaded = false;
+
+        public String Orientation
+        {
+            get { return (String)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contador.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("Orientation", typeof(String), typeof(UIRaiting), new PropertyMetadata(0, OrientationChangedCallbackStatic));
+
+
+        private static void OrientationChangedCallbackStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIRaiting control = (UIRaiting)d;
+            control.OrientationChangedCallback(d, e);
+        }
+
+        private void OrientationChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIRaiting control = (UIRaiting)d;
+            Debug.WriteLine("Orientation: " + control.Orientation);
+            if(control.Orientation.Equals("Vertical")) 
+            {
+                stackPanel.Orientation = Windows.UI.Xaml.Controls.Orientation.Vertical;
+            }
+            else
+            {
+                stackPanel.Orientation = Windows.UI.Xaml.Controls.Orientation.Horizontal;
+            }
+        }
+
+        public int Contador
+        {
+            get { return (int)GetValue(ContadorProperty); }
+            set { SetValue(ContadorProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Contador.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ContadorProperty =
+            DependencyProperty.Register("Contador", typeof(int), typeof(UIRaiting), new PropertyMetadata(0, ContadorChangedCallbackStatic));
+
+
+        private static void ContadorChangedCallbackStatic(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIRaiting control = (UIRaiting)d;
+            control.ContadorChangedCallback(d, e);
+        }
+
+        private void ContadorChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UIRaiting control = (UIRaiting)d;
+            tbContador.Text = control.Contador.ToString();
+            CangeOpacityContador();
+        }
+
+        private void CangeOpacityContador()
+        {
+            if (loaded)
+            {
+
+
+                for (int i = 1; i <= CAMELOS; i++)
+                {
+                    if (i <= Contador)
+                    {
+                        stackPanel.Children[i - 1].Opacity = 1;
+                    }
+                    else
+                    {
+                        stackPanel.Children[i - 1].Opacity = 0.3;
+
+                    }
+                }
+
+            }
+        }
+
+        private void UserControl_Loading(FrameworkElement sender, object args)
+        {
+
+            for(int i = 1; i <= CAMELOS; i++)
+            {
+                Image img = new Image();
+                BitmapImage bi = new BitmapImage();
+                bi.UriSource = new Uri("ms-appx://20241107_RaitingControl/Assets/camelo_nano_transparente.png");
+                img.Source = bi;
+                img.Height = 30;
+                
+                if(i >= Contador)
+                {
+                    img.Opacity = 0.3;
+                }
+
+                img.Tag = i;
+                //Se añaden los eventos
+                img.PointerMoved += Img_PointerMoved;
+                img.PointerExited += Img_PointerExited;
+                img.Tapped += Img_Tapped;
+
+                stackPanel.Children.Add(img);
+            }
+
+            loaded = true;
+            CangeOpacityContador();
+        }
+
+        private void Img_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            Image img = (Image)sender;
+            Contador = (int)img.Tag;
+            
+        }
+
+        private void Img_PointerMoved(object sender, PointerRoutedEventArgs e)
+        {
+            ChangeOpacity(sender, 0.6);
+        }
+        private void Img_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            ChangeOpacity(sender, 0.3);
+        }
+
+        private void ChangeOpacity(object sender, double opacity)
+        {
+            Image img = (Image)sender;
+
+            for (int i = 1; i <= (int)img.Tag; i++)
+            {
+                if (stackPanel.Children[i - 1].Opacity < 1)
+                {
+                    stackPanel.Children[i - 1].Opacity = opacity;
+                }
+
+            }
+        }
+
+        private void Button_Click_Remove(object sender, RoutedEventArgs e)
+        {
+            Contador = 0;
+        }
+    }
+}
