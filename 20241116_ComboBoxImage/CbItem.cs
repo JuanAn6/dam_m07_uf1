5798a1db36a83c0c41601e368f206777dbf8abc6
JuanAn6 <juanantoniorizija666@gmail.com>
2024-11-18 16:53:03 +0100

    18-11-2024

---
+++ 20241116_ComboBoxImage/CbItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Controls;
+
+namespace _20241116_ComboBoxImage
+{
+    internal class CbItem
+    {
+        public string Img { get; set; }
+        public string Text { get; set; }
+
+        public CbItem(string img, string text)
+        {
+            this.Img = img;
+            this.Text = text;
+        }
+
+    }
+}
