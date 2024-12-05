fa1d93654df4309edb512dff08fbc9f5e8dfd1cc
JuanAn6 <juanantoniorizija666@gmail.com>
2024-12-05 20:04:32 +0100

    05-12-2024

---
+++ BDDemo/Model/model/Dept.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.model
+{
+    public class Dept
+    {
+        
+
+        private int dept_no;
+
+        public int Dept_no
+        {
+            get { return dept_no; }
+            set { dept_no = value; }
+        }
+
+        private string dNom;
+
+        public string DNom
+        {
+            get { return dNom; }
+            set { dNom = value; }
+        }
+
+        private string loc;
+
+        public string Loc
+        {
+            get { return loc; }
+            set { loc = value; }
+        }
+
+        public Dept(int dept_no, string dNom, string loc)
+        {
+            Dept_no = dept_no;
+            DNom = dNom;
+            Loc = loc;
+        }
+
+
+    }
+}
